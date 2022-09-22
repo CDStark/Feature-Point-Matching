73aa27bad8cd5d7f5a981530cca60af427cd4f8e
Christoph Staerk <christoph.d.staerk@t-online.de>
2022-09-22 16:59:48 +0200

    Nicer plots

--- test_fourier.py
+++ test_fourier.py
@@ -5,9 +5,13 @@ import scipy.ndimage as ndi
 import matplotlib.pyplot as plt
 import numpy as np
 
-img = cv2.cvtColor(cv2.imread('./Data/SB_20220228_007_001.tiff'), cv2.COLOR_BGR2RGB)
+img = cv2.cvtColor(cv2.imread('./Data/SB_20220228_007_070.tiff'), cv2.COLOR_BGR2RGB)
 
-plt.figure(dpi=1000)
+fig, ax = plt.subplots(2, 2, dpi=1000)
+ax[0, 0].set_title('Fourier spectrum')
+ax[0, 1].set_title('Fourier spectrum filtered')
+ax[1, 0].set_title('Image')
+ax[1, 1].set_title('Image filtered')
 
 fft_imgr = np.fft.fft2(img[:, :, 0])
 fft_imgr = scipy.fft.fftshift(fft_imgr)
@@ -16,8 +20,7 @@ fft_imgg = scipy.fft.fftshift(fft_imgg)
 fft_imgb = np.fft.fft2(img[:, :, 2])
 fft_imgb = scipy.fft.fftshift(fft_imgb)
 
-plt.imshow(np.log(np.absolute(fft_imgb)), cmap='magma', aspect='auto')
-plt.show()
+ax[0, 0].imshow(np.log(np.absolute(fft_imgb)), cmap='magma', aspect='auto')
 
 angle = 0.1
 for k in range(img.shape[1]):
@@ -33,19 +36,18 @@ for k in range(img.shape[1]):
             fft_imgb[j, k] = 0
 
 
-plt.imshow(np.log(np.absolute(fft_imgb)), cmap='magma', aspect='auto')
-plt.show()
+ax[0, 1].imshow(np.log(np.absolute(fft_imgb)), cmap='magma', aspect='auto')
 
 
-fig, ax = plt.subplots(1, 2, dpi=1000)
-ax[0].imshow(img)
+ax[1, 0].imshow(img)
 
 orig_imgr = np.fft.ifft2(scipy.fft.fftshift(fft_imgr))
 orig_imgg = np.fft.ifft2(scipy.fft.fftshift(fft_imgg))
 orig_imgb = np.fft.ifft2(scipy.fft.fftshift(fft_imgb))
-ax[1].imshow(np.stack((orig_imgr, orig_imgg, orig_imgb), axis=-1).real.astype(int))
+ax[1, 1].imshow(np.stack((orig_imgr, orig_imgg, orig_imgb), axis=-1).real.astype(int))
 plt.show()
 exit()
+# ------------------------------------------------------------------------------
 
 fig, ax = plt.subplots(nrows=2, ncols=1)
 ax[0].imshow(img)
