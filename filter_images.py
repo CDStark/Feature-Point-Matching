d288ffaaf7a56e7106b08d89f69e05d6e39162c8
Christoph Staerk <christoph.d.staerk@t-online.de>
2022-10-10 16:39:06 +0200

    Include smoothing of image to avoid jagged lines

--- filter_images.py
+++ filter_images.py
@@ -64,44 +64,66 @@ if bool(config.getboolean('Red Line', 'active')):
         path = path.parent / (path.stem + '_WL' + path.suffix)
         img = cv2.cvtColor(cv2.imread(str(path)), cv2.COLOR_BGR2RGB)
     # prepare plots
-    fig, ax = plt.subplots(ncols=3, dpi=1000)
-    ax[0].set_title('Original image')
-    ax[1].set_title('Distance image')
-    ax[2].set_title('Red filtered image')
+    fig, ax = plt.subplots(ncols=3, nrows=2, dpi=1000)
+    ax[0, 0].set_title('Original image')
+    ax[0, 1].set_title('Distance image')
+    ax[0, 2].set_title('Red filtered image')
 
     y, x = config.getint('Red Line', 'y'), config.getint('Red Line', 'x')
-    ax[0].imshow(img)
-    ax[0].plot(x, y, 'r+')
+    ax[0, 0].imshow(img)
+    ax[0, 0].plot(x, y, 'r+')
 
     red = img[y, x, :]
     threshold = config.getfloat('Red Line', 'threshold')
     max_brightness = config.getint('Red Line', 'max_brightness')
+    smoothing_sdv = config.getint('Red Line', 'smoothing_sdv')
+
+    # Create images representing similarity to selected color and overall brightness
     shape = img.shape
     distance_img = np.empty((shape[0], shape[1]))
     filtered_img = np.empty((shape[0], shape[1]))
+    brightness_img = np.empty((shape[0], shape[1]))
     for r in range(shape[0]):
         for c in range(shape[1]):
             color_distance = np.linalg.norm(red - img[r, c, :])
             distance_img[r, c] = color_distance
-            brightness = np.mean(img[r, c, :])
-            right_color = color_distance < threshold \
-                and brightness < max_brightness
-            filtered_img[r, c] = 1 if right_color else 0
-    filtered_img_grey = np.stack((filtered_img, filtered_img, filtered_img), axis=2)
+            brightness_img[r, c] = np.mean(img[r, c, :])
+            # right_color = color_distance < threshold \
+            #     and brightness < max_brightness
+            # filtered_img[r, c] = 1 if right_color else 0
+    ax[1, 0].imshow(brightness_img)  # temp
 
-    ax[1].imshow(distance_img)
-    ax[2].imshow(filtered_img_grey)
+    # Blurr feature images to reduce rageddness of the lines
+    distance_img_blurred = ndi.gaussian_filter(distance_img, 4)
+    brightness_img_blurred = ndi.gaussian_filter(brightness_img, 4)
+
+    # create filtered image based on feature images
+    brightness_mask = brightness_img < max_brightness
+    distance_mask = distance_img < threshold
+    filtered_img = np.zeros_like(distance_img)
+    filtered_img[np.bitwise_and(brightness_mask, distance_mask)] = 1.0
+
+    brightness_mask = brightness_img_blurred < max_brightness
+    distance_mask = distance_img_blurred < threshold
+    filtered_img_blurred = np.zeros_like(distance_img_blurred)
+    filtered_img_blurred[np.bitwise_and(brightness_mask, distance_mask)] = 1.0
+
+    # for plotting
+    filtered_img_grey = np.stack((filtered_img, filtered_img, filtered_img), axis=2)
+    filtered_img_grey_blurred = np.stack((filtered_img_blurred, filtered_img_blurred, filtered_img_blurred), axis=2)
+
+    # plot data
+    ax[0, 1].imshow(distance_img)
+    ax[0, 2].imshow(filtered_img_grey)
+
+    ax[1, 1].imshow(distance_img_blurred)
+    ax[1, 2].imshow(filtered_img_grey_blurred)
     plt.show()
-    if config.getboolean('Red Line', 'smooth'):
-        distance_img_blurred = ndi.gaussian_filter(distance_img, 4)
-        filtered_img_grey_blurred = np.where(distance_img_blurred > threshold, 1.0, 0.0)
-        fig, ax = plt.subplots(ncols=3, dpi=1000)
-        ax[1].imshow(distance_img_blurred)
-        ax[2].imshow(np.stack((filtered_img_grey_blurred, filtered_img_grey_blurred, filtered_img_grey_blurred), axis=2))
-        plt.show()
 
     # save data
-    np.savetxt(path.parent / (path.stem + '_sceleton.csv'), filtered_img, fmt='%1i', delimiter=',')
+    if config.getboolean('Red Line', 'smooth'):
+        np.savetxt(path.parent / (path.stem + '_sceleton.csv'), filtered_img_grey_blurred, fmt='%1i', delimiter=',')
+
     # fig.savefig(path.parent / (path.stem + '_filter_plot'))
 
 # save config used
